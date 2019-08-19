1d0c37642837f88fcbb8948c7ddb08951d500c21
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-08-19 21:53:58 +0100

    WIP: setup test for 'cannot be closed or opened if it is locked'

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer'; 
 import { render } from '@testing-library/react'
+import 'jest-dom/extend-expect'
 
 import Controls from './Controls'
 
@@ -9,5 +10,19 @@ describe('<Controls />', () => {
     const tree = renderer.create(<Controls />); 
 
     expect(tree.toJSON()).toMatchSnapshot();
+  })
+})
+
+describe('<Controls />', () => {
+  it('cannot be closed or opened if it is locked', () => {
+    const { getByText } = render(
+      <Controls 
+      locked='locked'
+      closed='closed'
+      />
+    )
+
+    const openGate = getByText(/^Open Gate$/i)
+    expect(openGate.closest('button')).toHaveAttribute('disabled');
   })
 })
