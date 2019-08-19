881049b6e8a61ec2a7a6b567cf8cd6ac2a433b66
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-08-19 20:57:18 +0100

    WIP: setup snapshots for Dashboard, Display, & Controls components

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,13 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer'; 
+import { render } from '@testing-library/react'
+
+import Controls from './Controls'
+
+describe('<Controls />', () => {
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Controls />); 
+
+    expect(tree.toJSON()).toMatchSnapshot();
+  })
+})
